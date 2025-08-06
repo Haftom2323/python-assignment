54e8b2eed2e25fe8d64f84c670a25832ecb6ba3f
haftom2323 <haftomtek23@gmail.com>
2025-08-06 03:53:14 +0300

    simple calculator added

---
+++ calculator.py
@@ -0,0 +1,11 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+operation = input("Enter the operation: ") # +, -, *, /
+
+if operation == "+":
+    print(num1 + num2)
+elif operation == "-":
+    print(num1 - num2)
+elif operation == "*":
+    print(num1 * num2)
